30275ea90b8a35ea2f64aa27981f8f54a0e5482a
Jonathan Luk <jluk87@gmail.com>
2018-10-07 17:20:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class App < Sinatra::Base
     "My name is "
   end
 
-  get 'hometown' do
+  get '/hometown' do
     "My hometown is "
   end
 
-  get 'favorite-song' do
+  get '/favorite-song' do
     "My favorite song is "
   end
 
